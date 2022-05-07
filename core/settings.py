52f6763320e7578d8e9ba2f87d127aa08fcc860f
Paul Odhiambo <paulodhiambo962@gmail.com>
2022-05-07 21:22:46 +0300

    update

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 import os
 from decouple import config
+from django.template.context_processors import media
 from unipath import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,6 +120,5 @@ STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'apps/static'),
 )
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
 #############################################################
 #############################################################
--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,3 @@ urlpatterns = [
     path("", include("apps.authentication.urls")),  # Auth routes - login / register
     path("", include("apps.home.urls"))  # UI Kits Html files
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
