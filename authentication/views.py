14e805399fa633b974c29b76487b230ca9fb5818
Paul Odhiambo <paulodhiambo962@gmail.com>
2022-05-08 10:21:50 +0300

    update

--- authentication/views.py
+++ authentication/views.py
@@ -25,7 +25,7 @@ def login_view(request):
         else:
             msg = 'Error validating the form'
 
-    return render(request, "accounts/../templates/accounts/login.html", {"form": form, "msg": msg})
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
 
 def register_user(request):
@@ -50,4 +50,4 @@ def register_user(request):
     else:
         form = SignUpForm()
 
-    return render(request, "accounts/../templates/accounts/register.html", {"form": form, "msg": msg, "success": success})
+    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 def index(request):
     context = {'segment': 'index'}
 
-    html_template = loader.get_template('home/../templates/home/index.html')
+    html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
 
 
@@ -31,9 +31,9 @@ def pages(request):
 
     except template.TemplateDoesNotExist:
 
-        html_template = loader.get_template('home/../templates/home/page-404.html')
+        html_template = loader.get_template('home/page-404.html')
         return HttpResponse(html_template.render(context, request))
 
     except:
-        html_template = loader.get_template('home/../templates/home/page-500.html')
+        html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
